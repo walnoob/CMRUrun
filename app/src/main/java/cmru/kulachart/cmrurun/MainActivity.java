b0d6ae886d4e7150e36f12e7747b6d9361ab92ad
walnoob <walnut@live.com>
2016-06-27 15:07:49 +0700

    Set Botton And Avatar

--- app/src/main/java/cmru/kulachart/cmrurun/MainActivity.java
+++ app/src/main/java/cmru/kulachart/cmrurun/MainActivity.java
@@ -1,7 +1,9 @@
 package cmru.kulachart.cmrurun;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+    }//main medhod
+
+    public void clickSignUpMain(View view) {
+        startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+    }//clicksignupmain
+} // main class
