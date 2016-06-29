85cf428d3e8e73c039c68d5a25820061d7cc7947
walnoob <walnut@live.com>
2016-06-29 15:38:42 +0700

    Map location lat lon compleate

--- app/src/main/java/cmru/kulachart/cmrurun/MapsActivity.java
+++ app/src/main/java/cmru/kulachart/cmrurun/MapsActivity.java
@@ -1,46 +1,147 @@
 package cmru.kulachart.cmrurun;
 
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private double cmruLatADouble = 18.806007 , cmruLonADouble = 98.986017;
+    private double userLatADouble, userLngADouble;
+    private LocationManager locationManager;
+    private Criteria criteria;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+        setContentView(R.layout.my_layout);
+
+        //setup
+        userLatADouble = cmruLatADouble;
+        userLngADouble = cmruLonADouble;
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+    }//main method
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        locationManager.removeUpdates(locationListener);
+
+
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        locationManager.removeUpdates(locationListener);
+        Location networkLocation = myFindLocation(LocationManager.NETWORK_PROVIDER);
+        if (networkLocation != null) {
+            userLatADouble = networkLocation.getLatitude();
+            userLngADouble = networkLocation.getLongitude();
+        }//if
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+        Location gpsLocation = myFindLocation(LocationManager.GPS_PROVIDER);
+        if (gpsLocation != null) {
+            userLatADouble = gpsLocation.getLatitude();
+            userLngADouble = gpsLocation.getLongitude();
+        }
+
+        Log.d("29JuneV1", "userLat ==>" + userLatADouble);
+        Log.d("29JuneV1", "userLon ==>" + userLngADouble);
+    }//onresum
+
+    public Location myFindLocation(String strProvider) {
+
+        Location location = null;
+        if (locationManager.isProviderEnabled(strProvider)) {
+            locationManager.requestLocationUpdates(strProvider, 1000, 10,locationListener);
+            location = locationManager.getLastKnownLocation(strProvider);
+
+        } else {
+            Log.d("29June","Cannot Find Location");
+        }
+
+        return location;
+    }
+
+    public LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+
+            userLngADouble = location.getLatitude();
+            userLngADouble = location.getLongitude();
+
+        }//on location chang
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
+
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
-}
+       //set to cmru
+        LatLng latLng = new LatLng(cmruLatADouble, cmruLonADouble);
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16));
+
+        //
+        createStationMaker();
+
+    }//onmap ready
+
+    private void createStationMaker() {
+        MyData myData = new MyData();
+        double[] latDoubles = myData.getLatStationDoubles();
+        double[] lngDoubles = myData.getLngStationDoubles();
+        int[] iconInts = myData.getIconStationInts();
+
+        for (int i=0;i<latDoubles.length;i++) {
+            LatLng latLng = new LatLng(latDoubles[i], lngDoubles[i]);
+            mMap.addMarker(new MarkerOptions().position(latLng)
+                    .icon(BitmapDescriptorFactory.fromResource(iconInts[i]))
+                    .title("ด่านที่ " + Integer.toString(i + 1)));
+
+        }
+    }//createStationMaker
+
+
+} //mainclass
--- app/src/main/java/cmru/kulachart/cmrurun/MyData.java
+++ app/src/main/java/cmru/kulachart/cmrurun/MyData.java
@@ -9,8 +9,25 @@ public class MyData {
             R.drawable.bird48, R.drawable.doremon48, R.drawable.kon48, R.drawable.nobita48, R.drawable.rat48
     };
 
+    private double[] latStationDoubles = new double[]{18.807690, 18.805526, 18.807821, 18.807659};
+    private double[] lngStationDoubles = new double[]{98.985719, 98.985810, 98.987419, 98.988674};
+
+    private int[] iconStationInts = new int[]{R.drawable.build1, R.drawable.build2, R.drawable.build3, R.drawable.build4};
+
+    public int[] getIconStationInts() {
+        return iconStationInts;
+    }
+
     public int[] getAvatarInts() {
 
         return avatarInts;
+    }
+
+    public double[] getLatStationDoubles() {
+        return latStationDoubles;
+    }
+
+    public double[] getLngStationDoubles() {
+        return lngStationDoubles;
     }
 }//mydata main class
